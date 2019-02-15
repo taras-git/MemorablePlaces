a983bf2a720af4c39a9c261ea7f1bf7fc3b7ad29
Taras Tymchyshyn <tt@Tarass-Mac-mini.local>
2019-02-15 15:23:11 +0200

    add dictonary places

--- MemorablePlaces/TableViewController.swift
+++ MemorablePlaces/TableViewController.swift
@@ -7,28 +7,48 @@
 //
 
 import UIKit
+import CoreLocation
 
-var list: [String] = ["Taj Mahal", "Lviv"]
+var defaultPlacesList: [String : CLLocationCoordinate2D] =
+    ["Taj Mahal" : CLLocationCoordinate2D(latitude: 21.1, longitude: 78.0),
+    "Lviv": CLLocationCoordinate2D(latitude: 21.1, longitude: 78.0)]
+
+struct Places {
+    var placeName : String!
+    var placeCoordinate : CLLocationCoordinate2D!
+}
 
 class TableViewController: UITableViewController {
 
+    var placesArray = [Places]()
+    
+    fileprivate func getPlacesArray() -> [Places] {
+        for (key, value) in defaultPlacesList {
+            print("\(key) -> \(value)")
+            placesArray.append(Places(placeName: key, placeCoordinate: value))
+        }
+        print(defaultPlacesList)
+        return placesArray
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
         
-        print(list)
+        getPlacesArray()
 
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
+        return placesArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = list[indexPath.row]
+//        var key  = Array(placesArray)[indexPath.row]
+        cell.textLabel?.text = placesArray[indexPath.row].placeName!
         
         return cell
     }
@@ -39,7 +59,8 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            list.remove(at: indexPath.row)
+//            list.remove(at: indexPath.row)
+            placesArray.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
